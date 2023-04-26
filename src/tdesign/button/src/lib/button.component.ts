24c302b3b110ed5056e2951ed48e9cfb1bad5f79
scung-cn <y-y-liu@hotmail.com>
2023-04-26 22:33:52 +0800

    add project - add button

---
+++ src/tdesign/button/src/lib/button.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  template: `
+    <p>
+      button works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ButtonComponent {
+
+}
