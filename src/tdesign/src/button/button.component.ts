d76f23fc8c3a4a88752b46e38ef8e7c16639a40b
scung-cn <y-y-liu@hotmail.com>
2023-04-27 21:26:03 +0800

    modify Adjust the schema

---
+++ src/tdesign/src/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 't-button',
+  templateUrl: './botton.component.html',
+  styleUrls: [ './botton.component.scss' ]
+})
+export class ButtonComponent {
+
+}
---
+++ src/tdesign/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of button
+ */
+
+export * from './button/button.service';
+export * from './button/button.component';
+export * from './button/button.module';
