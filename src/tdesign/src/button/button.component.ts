cb3bb39787bbe63d4b67b6e6b521f743c27638b8
scung-cn <y-y-liu@hotmail.com>
2023-05-06 22:28:30 +0800

    add button attribute

--- src/tdesign/src/button/button.component.ts
+++ src/tdesign/src/button/button.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 't-button',
+  selector: 'botton[t-button]',
   templateUrl: './botton.component.html',
   styleUrls: [ './botton.component.scss' ]
 })
 export class ButtonComponent {
+  @Input()
+  theme: '' | 'primary' | 'success' | 'dange' | 'warning'= 'primary';
 
+  @Input()
+  variant:'base'|'outline'|'dashed'|'text'='base';
+
+  @Input()
+  disable=false;
+
+  @Input()
+  loading=false;
+
+  @Input()
+  size:'small'|'medium'|'large' ='medium';
+  
+  @Input()
+  shape:'reactangle'|'square'|'round'|'circle'='reactangle';
 }
